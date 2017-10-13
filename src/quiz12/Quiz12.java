1a6bc6fd6213a30d0ebe230a4317fd02a68be75a
Adars <Adars@LAPTOP-5M1MF8QA>
2017-10-12 22:28:46 -0400

    Finished

--- src/quiz12/Quiz12.java
+++ src/quiz12/Quiz12.java
@@ -15,62 +15,25 @@ public class Quiz12 {
     /**
      * @param args the command line arguments
      */
-    public static String reverseString(String word){
-        //variables
-        char character = ',';
-        char hold = ',';
-        int length = word.length();
+    public String reverseString(String word) {
+        //base case  
+        if ((word == null) || (word.length() <= 1)) 
+            return word;
         
-        //break word into each letter
-        for (int i = 0; i < length; i++) {
-         character = word.charAt(i);
-        }
-         
-        //base case
-        if(length == 1){
-         return word;   
-        }else{
-            //start from end of the word 
-            for (int i = length - 1 ; i < length; i--) {
-             hold = word.charAt(i);
-                
-            return "" + hold;
-            }
-        }
-          return "false";
-    
+
+        // replace first letter and add it at the end. 
+        return reverseString(word.substring(1)) + (word.charAt(0));
+
     }
-    
-    
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-   
-        System.out.println("Enter a Word");
-        String word = input.nextLine();
-        
-        String reverse = reverseString(word);
-        System.out.println(reverse);
-         
-   
-//        //NOT RECURSIVE
-//        System.out.println("Enter a Word");
-//        String wordn = input.nextLine();
-//     
-//      //variables
-//        char character = ',';
-//        char hold = ',';
-//        int length = word.length();
-//        //break word into each letter
-//        for (int i = 0; i < length; i++) {
-//         character = word.charAt(i);
-//        }
-//        //space
-//        System.out.println("");
-//        
-//       //start from 
-//            for (int i = length - 1 ; i < length; i--) {
-//             hold = word.charAt(i);
-//                System.out.print(hold);
-//                }
+        //Testing mehtod
+        Quiz12 test = new Quiz12();
+
+        //Word 
+        String word = "Hello";
+        System.out.println("The Reverse String of " + word + " is:");
+        System.out.println(test.reverseString(word));
+
     }
 }
